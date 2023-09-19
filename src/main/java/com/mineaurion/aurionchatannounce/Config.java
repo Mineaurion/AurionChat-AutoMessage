5f7168052d099ab1d70ce045e3de49ecdeff81a5
Yann <yann24.bonnau@hotmail.fr>
2023-09-19 23:06:30 +0200

    fix: config default loaded anyway & plugin.yml

--- src/main/java/com/mineaurion/aurionchatannounce/Config.java
+++ src/main/java/com/mineaurion/aurionchatannounce/Config.java
@@ -10,7 +10,6 @@ public class Config {
     public FileConfiguration config;
 
     public Config(AurionchatAutoMessage plugin) {
-        plugin.getConfig().options().copyDefaults(true);
         plugin.saveDefaultConfig();
         config = plugin.getConfig();
     }
